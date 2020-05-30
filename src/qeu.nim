9777643fc1323365ec6a9d1d46d4ad49c9598985
hyu1996 <164346864@qq.com>
2020-05-30 21:22:24 +0800

    Unified naming style

--- src/qeu.nim
+++ src/qeu.nim
@@ -31,9 +31,9 @@ template castToBiggestNumber[T: Number](number: T, kind: SymbolKind): auto =
     int64(number)
 
 template compareTwoNumber[T: Number](number1: T, number2: T, kordering: Ordering): untyped =
-  when kordering == Ordering.LESS:
+  when kordering == Ordering.Less:
     number1 < number2
-  elif kordering == Ordering.GREATER:
+  elif kordering == Ordering.Greater:
     number1 > number2
   else:
     number1 == number2
@@ -54,12 +54,12 @@ template compareTwoNumber[T: Number, E: Number](number1: T, number2: E, greater:
   when isUnsignedNumber(typeof(T)) and isUnsignedNumber(typeof(E)):
     compare(number1, number2, SymbolKind.UnsignedSymbol)
   elif isUnsignedNumber(typeof(T)):
-    if number2 < cast[E](0):
+    if number2 < E(0):
       result = greater
     else:
       compare(number1, number2, SymbolKind.UnsignedSymbol)
   elif isUnsignedNumber(typeof(E)):
-    if number1 < cast[T](0):
+    if number1 < T(0):
       result = less
     else:
       compare(number1, number2, SymbolKind.UnsignedSymbol)
@@ -70,7 +70,7 @@ proc less*[T: Number, E: Number](
   number1: T, number2: E): bool {.inline, noSideEffect.} = 
 
   template checkLess[T: Number, E: Number](number1: T, number2: E, kind: SymbolKind): untyped =
-    result = compareTwoNumber(number1, number2, kind, Ordering.LESS)
+    result = compareTwoNumber(number1, number2, kind, Ordering.Less)
     
   compareTwoNumber(number1, number2, false, true, checkLess)
 
@@ -82,7 +82,7 @@ proc equal*[T: Number, E: Number](
   number1: T, number2: E): bool {.inline, noSideEffect.} = 
 
   template checkEqual[T: Number, E: Number](number1: T, number2: E, kind: SymbolKind): untyped =
-    result = compareTwoNumber(number1, number2, kind, Ordering.EQUAL)
+    result = compareTwoNumber(number1, number2, kind, Ordering.Equal)
     
   compareTwoNumber(number1, number2, false, false, checkEqual)
 
@@ -96,11 +96,11 @@ proc compare*[T: Number, E: Number](
   number1: T, number2: E): Ordering {.inline, noSideEffect.} =
 
   template checkOrdering[T: Number, E: Number](number1: T, number2: E, ksymbol: SymbolKind): untyped =
-    if compareTwoNumber(number1, number2, ksymbol, Ordering.EQUAL):
-      result = Ordering.EQUAL
-    elif compareTwoNumber(number1, number2, ksymbol, Ordering.LESS):
-      result = Ordering.LESS
+    if compareTwoNumber(number1, number2, ksymbol, Ordering.Equal):
+      result = Ordering.Equal
+    elif compareTwoNumber(number1, number2, ksymbol, Ordering.Less):
+      result = Ordering.Less
     else:
-      result = Ordering.GREATER
+      result = Ordering.Greater
 
-  compareTwoNumber(number1, number2, Ordering.GREATER, Ordering.LESS, checkOrdering)
+  compareTwoNumber(number1, number2, Ordering.Greater, Ordering.Less, checkOrdering)
